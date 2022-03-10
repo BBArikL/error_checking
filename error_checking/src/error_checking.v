d6af01942ed0328a7dd5dd8bcd7f65d98152d5a7
Arik <BBArik@protonmail.com>
2022-03-10 17:36:18 -0500

    Move tasks check_function, local_answer, and generate_question into module StateMachine.

--- error_checking/src/error_checking.v
+++ error_checking/src/error_checking.v
@@ -18,45 +18,6 @@
 //
 //-----------------------------------------------------------------------------
 `timescale 1 ns / 1 ps
-	
-
-	
-//module QuestionGeneration(input clk);
-	
-	//reg [3:0] question;
-				
-	//assign question = 4; //genrerate a bits question
-		
-//endmodule
-
-//Module qui calcule la reponse  a la question localement
-//update la valeur de answer chaque fois que la question change
-module localAnswer(input[3:0] question, output[3:0] answer);
-		assign answer[0] = ~question[0];
-		assign answer[1] = question[0] ^ question[1];
-		assign answer[2] = question[1] ^ question[2];
-		assign answer[3] = question[2] ^ question[3];
-endmodule
-
-//Module qui verifie si la reponse local est la meme que celle du obc
-module CheckFunction(input[3:0] answerOBC, output result,input[3:0] question);
-
-	wire[3:0] answer;
-	
-	// Gets our local answer
-	localAnswer l1(question, answer);
-
-	// Define how we compare the two answers
-	function reg answerCompare(input [3:0] answer,input [3:0] answerOBC);
-		begin
-			answerCompare = (answer == answerOBC) ? 1 : 0;
-		end
-	endfunction
-	
-	// Compare answer and gives back result
-	assign result = answerCompare(answer,answerOBC);
-
-endmodule
 
 
 //high level module that communicates directly with OBC
@@ -85,13 +46,37 @@ module StateMachine(input reset,input clk);
 	localparam checking = 2'b01;
 	localparam valid = 2'b10;
 	localparam shutdown = 2'b11;//va devoir envoyer un signal au OBC 1 de shutdown et  allumer OBC2 
-	wire result; //Result
+	reg result; //Result
 	wire [3:0] answer = 3'b000;
 	wire [3:0] answerOBC = 3'b000;
-	
-	task check_function(input[3:0] answer, input[3:0] answerOBC, output result);
+	reg [3:0] question;
+
+	task generate_question(output[3:0] question);
 		begin
-			// Check_function
+			question = 3'b111; // TODO
+		end
+	endtask
+
+	//Module qui calcule la reponse  a la question localement
+	//update la valeur de answer chaque fois que la question change
+	task local_answer(input[3:0] question, output[3:0] answer);
+		begin
+			assign answer[0] = ~question[0];
+			assign answer[1] = question[0] ^ question[1];
+			assign answer[2] = question[1] ^ question[2];
+			assign answer[3] = question[2] ^ question[3];
+		end
+	endtask
+
+
+	task check_function(input[3:0] answer, input[3:0] answerOBC, input[3:0] question, output result);
+		begin
+	
+			// Gets our local answer
+			local_answer(question, answer);
+			
+			// Compare answer and gives back result
+			assign result = (answer == answerOBC) ? 1 : 0;
 		end
 	endtask
 	
@@ -107,7 +92,7 @@ module StateMachine(input reset,input clk);
 			case(present_state)
 				start:
 				begin
-					check_function(answer, answerOBC, result);
+					check_function(answer, answerOBC, question, result);
 					
 					if(result)
 						next_state = valid;
@@ -123,8 +108,10 @@ module StateMachine(input reset,input clk);
 					//2: envoyer serie de question si toute les autre son bonne passer a valid sinon un autre etat de validation de pattern
 					correctAnswerCount = 0;
 					for(i=0; i < 10; i = i + 1)begin
+						generate_question(question);
+
 						//envoyer question
-						check_function(answer, answerOBC, result);
+						check_function(answer, answerOBC, question, result);
 						
 						if(result)
 							correctAnswerCount = correctAnswerCount + 1;
